48124371c325907051c4d805d0fc234fefe04031
tohidul <roman20222324@gmail.comProfile imageroman20222324@gmail.com>
2024-06-01 20:21:04 +0600

    first commit

---
+++ name.js
@@ -0,0 +1,152 @@
+
+//! Quz - 1
+function celsiusToFahrenheit(celsius) {
+  return celsiusF = F= (C*9 /5) + 32. ;
+}
+
+console.log(celsiusToFahrenheit(0)); // 32
+console.log(celsiusToFahrenheit(25)); // 77
+
+
+//! Quz - 2
+// function isEven(num) {
+//   return (num%2)== 0 ;
+// }
+
+// console.log(isEven(4)); // true
+// console.log(isEven(7)); // false
+
+
+
+//! Quz - 3 
+// function sum(a, b) {
+//     return a + b ;
+//   }
+  
+//   console.log(sum(3, 4)); // 7
+//   console.log(sum(10, 20)); // 30
+  
+  // ! Quz - 4
+// let arr = [3,5,1,9 , -1, ,-5, 0, -10] 
+
+//   function findSmallestNum(arr) {
+//     return arr.min >= 0 ;
+
+//   }
+  
+//   console.log(findSmallestNum([3, 5, 1, 9])); // 1
+//   console.log(findSmallestNum([-1, -5, 0, 10])); // -5
+  
+  
+//   //! Quz - 5
+
+  
+//   function countVowels(str) {
+//     let vowels = 'aeiouAEIOU';
+//     return 
+  
+    
+//   }
+  
+//   console.log(countVowels("Javascript")); // 3
+//   console.log(countVowels("hello world")); // 3
+  
+  //! Quz - 6
+
+  // function getFirstElement(arr) {
+  //   return arr
+  // }
+  
+  // console.log(getFirstElement([1, 2, 3])); // 1
+  // console.log(getFirstElement(["a", "b", "c"])); // "a"
+  
+
+  // //! Quz - 7
+
+  // let arr [1,2,3]
+  // function isArrayEmpty(arr) {
+  //   return arr
+  // }
+  
+  // console.log(isArrayEmpty([])); // true
+  // console.log(isArrayEmpty([1, 2, 3])); // false
+
+  // //! Quz - 8
+
+  // function factorialize(num) {
+  //   if (num === 0 || num === 1) return 1;
+  //   return  num == i++
+  // }
+  
+  // console.log(factorialize(5)); // 120
+  // console.log(factorialize(7)); // 5040
+
+  
+  // //! Quz - 9
+
+  // function reverseString(str) {
+  //   return ...;
+  // }
+  
+  // console.log(reverseString("hello")); // "olleh"
+  // console.log(reverseString("world")); // "dlrow"
+
+  // //! Quz - 10
+  let str = "hello world "
+  // function toLowerCase(str) {
+    // return tolowercase(str)
+  // }
+  
+  // console.log(toLowerCase("HELLO WORLD")); // "hello world"
+  // console.log(toLowerCase("JavaScript")); // "javascript"
+
+  //! Quz - 11
+  
+  // function stringLength(str) {
+  //   return str + str - 5
+  // }
+  
+  // console.log(stringLength("hello")); // 5
+  // console.log(stringLength("world")); // 5
+
+  
+  //! Quz - 12
+
+  // function mergeArrays(arr1, arr2) {
+  //   return arr1 ,arr2;
+  // } mergeArrays(1,2,3,4,5,6, 'a','b','c' ,'d')
+  
+  // console.log(mergeArrays([1, 2, 3], [4, 5, 6])); // [1, 2, 3, 4, 5, 6]
+  // console.log(mergeArrays(["a", "b"], ["c", "d"])); // ["a", "b", "c", "d"]
+  
+//! Quz 13
+// let arr = [1,2,3,]
+// function getLastElement(arr) {
+//   return arr
+// }
+
+// console.log(getLastElement([1, 2, 3])); // 3
+// console.log(getLastElement(["a", "b", "c"])); // "c"
+
+//! Quz - 14
+// let h= "hello"
+// let w ='world'
+// let str = h + w ;
+// function getFirstCharacter(str) {
+//   return str
+// }
+
+// console.log(getFirstCharacter("hello")); // "h"
+// console.log(getFirstCharacter("world")); // "w"
+
+
+//! Quz - 15
+
+// let arr = [ 1,2,3,4, -1 , -2 , -3, -4 , 1.5, 2.5, 3.5]
+// function sumArray(arr) {
+//   return arr
+// }
+
+// console.log(sumArray([1, 2, 3, 4])); // 10
+// console.log(sumArray([-1, -2, -3, -4])); // -10
+// console.log(sumArray([1.5, 2.5, 3.5])); // 7.5
